e4decf8e5badc8defee21b83cc6dccecf6958627
Sergey Pankov <vacuum@ivacuum.ru>
2013-02-25 00:52:01 +0400

    Инициализация переехала в контейнер фреймворка

--- public_html/index.php
+++ public_html/index.php
@@ -8,14 +8,5 @@ namespace app;
 
 require('../../_/fw/master/bootstrap.php');
 
-$app['auth']->init($app['user']->data);
-$app['user']->setup();
-
-/* Домен временно закрыт для публики */
-if ($app['request']->header('Host') == 'dev.ivacuum.ru' && $app['user']['user_id'] != 1 && $app['user']->ip != '192.168.1.1' && $app['user']->ip != '79.175.20.190')
-{
-	// $app['request']->redirect(ilink('', 'http://ivacuum.ru'));
-}
-
 /* Маршрутизация запроса */
 $app['router']->_init()->handle_request();

8e95f3c8764ade6c83de993386f19ae5d4e3a731
Sergey Pankov <vacuum@ivacuum.ru>
2013-04-13 17:07:32 +0400

    Удаление ключей для восстановления пароля по расписанию

---
+++ includes/cron/newpasswd/purge.php
@@ -0,0 +1,28 @@
+<?php
+/**
+* @package ivacuum.ru
+* @copyright (c) 2013
+*/
+
+namespace app\cron\newpasswd;
+
+use fw\cron\task;
+
+/**
+* Чистка ключей для восстановления пароля
+*/
+class purge extends task
+{
+	public function run()
+	{
+		$sql = '
+			UPDATE
+				site_users
+			SET
+				user_newpasswd = ""';
+		$this->db->query($sql);
+		$this->log('Удалено ключей для восстановления пароля: ' . $this->db->affected_rows());
+		
+		return true;
+	}
+}

7c4b341b734d72359576a91d83a574b03a67306a
Sergey Pankov <vacuum@ivacuum.ru>
2013-08-27 00:48:01 +0400

    Упрощение удаления неактуальных кодов подтверждения

--- includes/cron/captcha/purge.php
+++ includes/cron/captcha/purge.php
@@ -15,30 +15,10 @@ class purge extends task
 {
 	public function run()
 	{
-		$sql = '
-			SELECT DISTINCT
-				c.session_id
-			FROM
-				site_confirm c
-			LEFT JOIN
-				site_sessions s ON (s.session_id = c.session_id)
-			WHERE
-				s.session_id IS NULL';
-		$result = $this->db->query($sql);
-		$sql_in = [];
+		$sql = 'DELETE FROM site_confirm WHERE expire < ?';
+		$this->db->query($sql, [$this->ctime]);
+		$this->log('Капч удалено: ' . $this->db->affected_rows());
 		
-		while ($row = $this->db->fetchrow($result))
-		{
-			$sql_in[] = (string) $row['session_id'];
-		}
-		
-		$this->db->freeresult($result);
-		
-		if (!empty($sql_in))
-		{
-			$sql = 'DELETE FROM site_confirm WHERE :session_ids';
-			$this->db->query($sql, [':session_ids' => $this->db->in_set('session_id', $sql_in)]);
-			$this->log('Капч удалено: ' . $this->db->affected_rows());
-		}
+		return true;
 	}
 }

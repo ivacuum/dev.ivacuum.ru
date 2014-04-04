f2da990bb25c48c3f546ca9941bd5175acca614a
Sergey Pankov <vacuum@ivacuum.ru>
2014-04-04 13:21:55 +0400

    Исправление настроек подключения к БД

--- config.sample.php
+++ config.sample.php
@@ -2,21 +2,21 @@
 
 $app = array_merge($app, [
 	/* Настройки подключения к БД */
-	'db.options' => [
+	'db.options' => array_merge($app['db.options'], [
 		'host' => 'localhost',
 		'name' => '',
 		'user' => '',
 		'pass' => '',
-	],
+	]),
+
+	'urls' => array_merge($app['urls'], [
+		'register' => '/ucp/register/',
+		'signin'   => '/ucp/signin/',
+		'signout'  => '/ucp/signout/',
+		'static'   => '',
+	]),
 ]);
 
 /* Настройки отладки */
 $app['errorhandler.options']['debug.ips'] = $app['profiler.options']['debug.ips'] = ['127.0.0.1', '::1'];
 $app['errorhandler.options']['email.404'] = '';
-
-/* Ссылки */
-$app['urls'] = array_merge($app['urls'], [
-	'register' => '/ucp/register/',
-	'signin'   => '/ucp/signin/',
-	'signout'  => '/ucp/signout/',
-]);
